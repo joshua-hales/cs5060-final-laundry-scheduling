b4b0cb5606ec81e0290e1f86594c7fcf2396816e
Joshua Hales <joshuadanielhales@gmail.com>
2023-04-28 22:33:25 -0600

    Add SchedulerFCFS and docs for Process

--- src/Process.py
+++ src/Process.py
@@ -1,15 +1,27 @@
 class Process:
     # TODO: Add support for multiple segments with different cycle times
     def __init__(self, name: int, start_time: int, cycle_time: int):
+        """
+        Represents a process that can be run on a segment
+        :param name: A unique identifier for the process
+        :param start_time: The arrival time of the process
+        :param cycle_time: The time it takes to complete the process
+        """
         self.__name = name
         self.__start_time = start_time
         self.__cycle_time = cycle_time
         self.__elapsed_time = 0
 
     def update(self):
+        """
+        Updates the current process for a step
+        """
         self.__elapsed_time += 1
 
     def is_complete(self):
+        """
+        :return: True if the process is complete, False otherwise
+        """
         return self.__elapsed_time >= self.__cycle_time
 
     def get_name(self):
@@ -19,4 +31,10 @@ class Process:
         return self.__start_time
 
     def get_remaining_time(self):
+        """
+        :return: The remaining time for the process to complete
+        """
         return self.__cycle_time - self.__elapsed_time
+
+    def get_elapsed_time(self):
+        return self.__elapsed_time
---
+++ src/Scheduler.py
@@ -0,0 +1,47 @@
+from Process import Process
+from Segment import Segment
+
+import collections
+
+
+class SchedulerFCFS:
+    def __init__(self, logger):
+        """
+        A first come first serve scheduler
+        :param logger: Object that implements a log method
+        """
+        self.__logger = logger
+        self.__queue = collections.deque()
+        self.__context_switches = 0
+
+    def update(self, process: Process, segment: Segment):
+        """
+        Updates the current process for a step
+        """
+        # TODO: Double check this logic
+        if process and process.is_complete():
+            # TODO: Log "process i complete on segment j"
+            # self.__logger.log()
+            self.__context_switches += 1
+            next_process = self.__queue.popleft() if self.__queue else None
+        else:
+            next_process = self.__queue.popleft() if self.__queue else None
+
+        if next_process and next_process.get_elapsed_time() == 0:
+            # TODO: Log "process i scheduled on segment j"
+            # self.__logger.log()
+            self.__context_switches += 1
+        return next_process
+
+    def notify(self, process: Process):
+        """
+        Adds a process to the ready queue
+        :param process: A Process
+        """
+        self.__queue.append(process)
+
+    def get_context_switches(self):
+        """
+        :return: The number of context switches that have occurred
+        """
+        return self.__context_switches
